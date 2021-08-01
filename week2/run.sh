f48a279280a25edcd9f3a820727265282ff3165d
Clayton Herbst <herbsca1@gmail.com>
2021-08-01 13:31:54 +0800

    Week 2 submission updates

--- week2/run.sh
+++ week2/run.sh
@@ -3,4 +3,6 @@
 export CP=.:./out:../lib/algs4.jar
 
 java -cp $CP -ea PercolationUnitTest PercolationUnitTest
-java -cp $CP -ea PercolationStatsUnitTest PercolationStatsUnitTest
+java -cp $CP -ea PercolationStats PercolationStats
+
+zip -j submission.zip */Percolation.java */PercolationStats.java && echo "...created submission.zip"
